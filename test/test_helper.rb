10d6bf9cbcf86649efe4c851fd7e21e8da1350b6
Alexey Osipenko <aratak@anahoret.com>
2011-12-18 23:59:50 +0200

    ActiveRecord is unnecessary.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
-ActionMailer::Base.delivery_method = :test
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.default_url_options[:host] = "test.com"
+# ActionMailer::Base.delivery_method = :test
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.default_url_options[:host] = "test.com"
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -16,7 +16,7 @@ Capybara.default_driver   = :rack_test
 Capybara.default_selector = :css
 
 # Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
+# ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
