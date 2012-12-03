5d464ee7bf2575f6d79b0f3e38676d583201067d
Alexander Sysuiev <alexander.sysuyev@anahoret.com>
2012-12-03 13:48:10 +0200

    Fix text-shadow scss

--- lib/wiskey/sass_extensions/functions/color_number.rb
+++ lib/wiskey/sass_extensions/functions/color_number.rb
@@ -2,7 +2,10 @@
 module Wiskey::SassExtensions::Functions::ColorNumber
 
   def color_number(color)
-    Sass::Script::String.new ie_hex_str(color).to_s.gsub(/^#/, '')
+    rgb_color = Sass::Script::Color::COLOR_NAMES[color.to_s] || (0..2).map {|n| color.to_s.gsub(/^#/, '').to_i(16) >> (n << 3) & 0xff}.reverse
+    sass_color = Sass::Script::Color.new rgb_color
+
+    Sass::Script::String.new sass_color.inspect.to_s.gsub(/^#/, '')
   end
 
 end
--- lib/wiskey/sass_extensions/functions/split_params.rb
+++ lib/wiskey/sass_extensions/functions/split_params.rb
@@ -2,7 +2,14 @@
 module Wiskey::SassExtensions::Functions::SplitParams
 
   def split_params(params)
-    Sass::Script::List.new params.to_s.split, :space
+    options = params.to_s.split.map do |atr|
+      result = Sass::Script::String.new(atr)
+      result = Sass::Script::Number.new(atr.to_i, [atr.gsub(atr.to_i.to_s, '')]) if atr.to_i == atr.to_f && !atr.to_i.zero?
+      result = Sass::Script::Number.new(atr.to_f, [atr.gsub(atr.to_i.to_s, '')]) if atr.to_i != atr.to_f && !atr.to_f.zero?
+      result
+    end
+
+    Sass::Script::List.new options, :space
   end
 
 end
